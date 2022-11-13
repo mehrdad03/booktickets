0e7575db24dbed19d26d0bc19dc1b577330d073b
LeGion <mehrdad.dadashi@gmail.com>
2022-11-13 23:44:18 +0300

    add input section to home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:booktickets/utils/app_styles.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
@@ -43,6 +44,23 @@ class HomeScreen extends StatelessWidget {
                               image: AssetImage("assets/images/img_1.png"))),
                     )
                   ],
+                ),
+                const Gap(25),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color(0xFFF4F6FD)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Row(
+                      children: [
+                        const Icon(FluentSystemIcons.ic_fluent_search_regular,
+                            color: Color(0xFFBFC205)),
+                       const Gap(5 ),
+                        Text("Search", style: Styles.headLineStyle4)
+                      ],
+                    ),
+                  ),
                 )
               ],
             ),
