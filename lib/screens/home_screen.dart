cffdd1d79c6fc07dd60c835a1f844b2ff2061564
LeGion <mehrdad.dadashi@gmail.com>
2022-11-14 00:00:26 +0300

    add upComing Flight row into home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,55 +14,69 @@ class HomeScreen extends StatelessWidget {
       body: ListView(
         children: [
           const Gap(40),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Good morning',
-                          style: Styles.headLineStyle3,
-                        ),
-                        const Gap(5),
-                        Text(
-                          'Book Tickets',
-                          style: Styles.headLineStyle1,
-                        )
-                      ],
-                    ),
-                    Container(
-                      width: 50,
-                      height: 50,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          image: const DecorationImage(
-                              image: AssetImage("assets/images/img_1.png"))),
-                    )
-                  ],
-                ),
-                const Gap(25),
-                Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: const Color(0xFFF4F6FD)),
-                  child: Padding(
-                    padding: const EdgeInsets.all(12),
-                    child: Row(
-                      children: [
-                        const Icon(FluentSystemIcons.ic_fluent_search_regular,
-                            color: Color(0xFFBFC205)),
-                       const Gap(5 ),
-                        Text("Search", style: Styles.headLineStyle4)
-                      ],
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Good morning',
+                            style: Styles.headLineStyle3,
+                          ),
+                          const Gap(5),
+                          Text(
+                            'Book Tickets',
+                            style: Styles.headLineStyle1,
+                          )
+                        ],
+                      ),
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: const DecorationImage(
+                                image: AssetImage("assets/images/img_1.png"))),
+                      )
+                    ],
+                  ),
+                  const Gap(25),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: const Color(0xFFF4F6FD)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Row(
+                        children: [
+                          const Icon(FluentSystemIcons.ic_fluent_search_regular,
+                              color: Color(0xFFBFC205)),
+                          const Gap(5),
+                          Text("Search", style: Styles.headLineStyle4)
+                        ],
+                      ),
                     ),
                   ),
-                )
-              ],
+                  const Gap(40),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Upcoming Flights", style: Styles.headLineStyle2),
+                      InkWell(
+                        onTap: (){
+                        },
+                          child: Text("View all",
+                              style: Styles.textStyle.copyWith(color: primary)))
+                    ],
+                  )
+                ],
+              ),
             ),
           )
         ],
